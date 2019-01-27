797df4c2b6db1c6d10eb397df3ad64c0e3731c7f
Vovoda <vovoda@orange.fr>
2019-01-27 02:36:12 +0100

    Map and ui

---
+++ Assets/CityUIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CityUIManager : MonoBehaviour
+{
+    public Text valueCarreau;
+    public Text valuePique;
+    public Text valueTrefle;
+    public Text valueCoeur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateValueCarreau(int _value)
+    {
+        valueCarreau.text = _value + " ♦";
+    }
+    public void UpdateValuePique(int _value)
+    {
+        valuePique.text = _value + " ♠";
+    }
+    public void UpdateValueTrefle(int _value)
+    {
+        valueTrefle.text = _value + " ♣";
+    }
+    public void UpdateValueCoeur(int _value)
+    {
+        valueCoeur.text = _value + " ♥";
+    }
+}
---
+++ Assets/Scripts/MapInitialization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class MapInitialization : MonoBehaviour
+{
+    public static int mapSize = 4;
+    string[] map;
+    int curMapIndex;
+
+    private void Start()
+    {
+        map = new string[mapSize];
+        curMapIndex = 0;
+    }
+
+    void Update()
+    {
+        // Get the Vuforia StateManager
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        // Iterate through the list of active trackables
+
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            //Debug.Log("Trackable: " + tb.TrackableName);
+
+            if (curMapIndex < mapSize)
+            {
+                if (Array.IndexOf(map, tb.TrackableName) == -1)
+                {
+                    map[curMapIndex] = tb.TrackableName;
+                    Debug.Log("Added : " + tb.TrackableName);
+                    curMapIndex++;
+                }
+            }
+            else
+            {
+                Debug.Log("Map is full");
+            }
+
+        }
+    }
+}
